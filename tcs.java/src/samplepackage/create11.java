ea0788f7a350986708135f10509301c461835eb6
sivas <sivas@S19>
2025-08-26 10:37:04 +0530

    i have completed!!!!!!!!!

---
+++ tcs.java/src/samplepackage/create11.java
@@ -0,0 +1,15 @@
+package samplepackage;
+
+public class create11 {
+
+	public create11() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		
+
+	}
+
+}
